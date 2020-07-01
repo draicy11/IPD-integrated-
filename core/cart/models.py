3bae96ef803d3d7221b38912f4fa6c0180cd2585
Siddharth <siddharthbharmoria@gmail.com>
2020-07-01 19:32:56 +0530

    added more cart functions

--- core/cart/models.py
+++ core/cart/models.py
@@ -5,3 +5,4 @@ from products.models import Product
 class MyCart(models.Model):
     user       = models.ForeignKey(User, on_delete= models.CASCADE)
     products   = models.ManyToManyField(Product)
+    total      = models.DecimalField(max_digits=10, decimal_places=2)
