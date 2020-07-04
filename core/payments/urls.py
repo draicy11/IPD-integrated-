0061b32e05c51fc97ec367a5fb69340e08f0858a
Siddharth <siddharthbharmoria@gmail.com>
2020-07-04 23:42:27 +0530

    added final payments

--- core/payments/urls.py
+++ core/payments/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from cart.views import final_cart
-from .views import checkout
+from .views import checkout, charge, success
 
 
 urlpatterns = [
-    path('payments/<str:token>', checkout, name= 'payment')
+    path('payments/<str:user_token>', checkout, name= 'payment'),
+    path('payments/<str:user_token>/charge', charge, name='charge'),
+    path('payments/<str:user_token>/success', success, name='success'),
+    
 ]
--- core/payments/views.py
+++ core/payments/views.py
@@ -1,23 +1,36 @@
-from django.shortcuts import render
-from cart.cart import get_user_cart
+from django.shortcuts import render, redirect
 import stripe
+from cart.cart import get_user_cart, create_cart
 
-stripe.api_key = 'sk_test_51H13KKBn1VxTvGlYZbmhbBxfjVXA2mjb0EtYtZhf7NWmjvlbsC3jLNyvdVk5W6hKSps7s5Hrh0ShiuihAN5j2qmS004QLBalKv'
 
-def checkout(request, token):
-    cart = get_user_cart(token)
-    total = int(cart.total)
+stripe.api_key = "sk_test_51H13KKBn1VxTvGlYZbmhbBxfjVXA2mjb0EtYtZhf7NWmjvlbsC3jLNyvdVk5W6hKSps7s5Hrh0ShiuihAN5j2qmS004QLBalKv"
 
-    intent = stripe.PaymentIntent.create(
+
+def checkout(request, user_token):
+    context={
+        'user_token' : user_token
+    }
+    return render(request , 'checkout.html', context)
+
+def charge(request, user_token):
+    cart = get_user_cart(user_token)
+    total = int(cart.total)*100
+    token = request.POST['stripeToken']
+    customer = stripe.Customer.create(
+        name = request.POST['name'],
+        email= request.POST['email'],
+        source=token,
+    )
+    charge = stripe.Charge.create(
     amount=total,
     currency='inr',
-    payment_method_types = ['card'],
-    # Verify your integration in this guide by including this parameter
-    metadata={'integration_check': 'accept_a_payment'},
+    description='Example charge',
+    customer=customer,
     )
-    context = {
-        'client_secret' : intent.client_secret,
+    return redirect('success', user_token )
 
-    } 
-
-    return render(request , 'checkout.html' , context)
+def success(request, user_token):
+    cart = get_user_cart(user_token)
+    cart.delete()
+    create_cart(user_token)
+    return render(request, 'success.html')
